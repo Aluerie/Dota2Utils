e04ce536f67d248219fdc0c24b7ad8880378e1dc
Aluerie❤ <aluerie@outlook.com>
2023-10-19 04:19:20 +0300

    Closes #3 + some brush-ups

--- BiweeklyQuestRefresh/biweekly_quest_refresh.py
+++ BiweeklyQuestRefresh/biweekly_quest_refresh.py
@@ -5,51 +5,42 @@ import pyautogui as pag
 import time
 import requests
 
-targets = log.StreamHandler(sys.stdout), log.FileHandler('_quests_refresh.logs')
-log.basicConfig(
-    format='%(message)s',
-    level=log.INFO,
-    handlers=targets
-)
+targets = log.StreamHandler(sys.stdout), log.FileHandler("_quests_refresh.logs")
+log.basicConfig(format="%(message)s", level=log.INFO, handlers=targets)
 
 # 1920x1080 resolution
 XY_progress = (729, 88)
 XY_refresh = (610, 842)
 
-def left_mouse_click(xy, wait_after=0, *, repeat=1, wait_repeat: float = 0.05):
+
+def left_mouse_click(xy, wait_after: float = 0, *, repeat=1, wait_repeat: float = 0.05):
     for _ in range(repeat):
         pag.moveTo(xy)
         time.sleep(0.001)
-        pag.mouseDown(xy, button='left')
+        pag.mouseDown(xy, button="left")
         time.sleep(0.001)
-        pag.mouseUp(xy, button='left')
+        pag.mouseUp(xy, button="left")
         time.sleep(wait_repeat)
     time.sleep(wait_after)
 
+
 def opendota_constants_heroes():
-    endpoint = 'https://api.opendota.com/api/constants/heroes'
+    endpoint = "https://api.opendota.com/api/constants/heroes"
     response = requests.get(endpoint)
-    heroes_names_list = [v['localized_name'] for k, v in response.json().items()]
+    heroes_names_list = [v["localized_name"] for k, v in response.json().items()]
     heroes_names_list.sort()
     return heroes_names_list
 
 
 def locate_turbo_quests():
-    item_images = [
-        'item_soul_ring.png',
-        'item_midas.png'
-    ]
+    item_images = ["item_soul_ring.png", "item_midas.png"]
     for item in item_images:
-        if pag.locateOnScreen(
-            f'./images/{item}',
-            confidence=0.9,
-            region=(950, 500, 100, 360)
-        ):
+        if pag.locateOnScreen(f"./images/{item}", confidence=0.9, region=(950, 500, 100, 360)):
             return True
 
 
 def refresh_dota_plus_quests():
-    log.info('--- Starting the Dota+ Refresh Script ---')
+    log.info("--- Starting the Dota+ Refresh Script ---")
     time.sleep(3)
     hero_list = opendota_constants_heroes()
     for name in hero_list:
@@ -60,9 +51,9 @@ def refresh_dota_plus_quests():
             log.info(name)
 
         time.sleep(2.0)
-        pag.press('right')
-    log.info('--- finished ---')
+        pag.press("right")
+    log.info("--- finished ---")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     refresh_dota_plus_quests()
--- D2RoshTimer/README.md
+++ D2RoshTimer/README.md
@@ -6,7 +6,6 @@ It is toned down `D2RoshTimer.exe` of [Fjara's project](https://github.com/Fjara
 
 I advice against using `.exe` file provided in this repo instead you should check original [Fjara's project](https://github.com/Fjara-h/D2RoshTimer) yourself.
 
-It uses GameState Integration to allow user to make a keybind 
-which quickly calculates Rosh timers and puts it into your Copypaste buffer.
+It uses GameState Integration to allow user to make a keybind which quickly calculates Rosh timers and puts it into your Copypaste buffer.
 
 I mainly posted it for "cloud" storage reasons.
